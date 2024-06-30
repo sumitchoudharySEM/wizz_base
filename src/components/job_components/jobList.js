eb19bcbc4f0c307e81f9146750a798f61b6780ae
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-07-01 05:07:47 +0530

    jobs posted by a user display completed

--- src/components/job_components/jobList.js
+++ src/components/job_components/jobList.js
@@ -1,13 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import JobCard from "./jobCard";
 import { useAccount, useWriteContract, useReadContract } from "wagmi";
 import { JOBS_CONTRACT_ADDRESS, PINATA_JWT } from "../../app/constants";
 import abi from "../../app/contract/jobsabi.json";
 import { usePathname } from "next/navigation";
+import { useParams } from "next/navigation";
 
 const JobList = () => {
-
   const pathname = usePathname().split("/");
+  const { username } = useParams();
   const [allJobs, setAllJobs] = useState();
 
   const { data, error } = useReadContract({
@@ -21,18 +22,26 @@ const JobList = () => {
     console.log("data:", data);
     console.log("error:", error);
     if (data !== undefined) {
-      if(pathname[1] == "opportunities") {
-        if(pathname[2] == "job") {
+      if (pathname[1] == "opportunities") {
+        if (pathname[2] == "job") {
           setAllJobs(data.filter((item) => item.jobType == "Job").reverse());
-        } else if(pathname[2] == "internship") {
-          setAllJobs(data.filter((item) => item.jobType == "Internship").reverse());
-        } else if(pathname[2] == "freelancing") {
-          setAllJobs(data.filter((item) => item.jobType == "Freelancing").reverse());
-        } else if(pathname[2] == "bounty") {
+        } else if (pathname[2] == "internship") {
+          setAllJobs(
+            data.filter((item) => item.jobType == "Internship").reverse()
+          );
+        } else if (pathname[2] == "freelancing") {
+          setAllJobs(
+            data.filter((item) => item.jobType == "Freelancing").reverse()
+          );
+        } else if (pathname[2] == "bounty") {
           setAllJobs(data.filter((item) => item.jobType == "Bounty").reverse());
         } else {
           setAllJobs(data.reverse());
         }
+      } else if (pathname[2] == "opportunities") {
+        setAllJobs(
+          data.filter((item) => item.employerUsername == username).reverse()
+        );
       } else {
         setAllJobs(data.reverse());
       }
@@ -41,15 +50,15 @@ const JobList = () => {
 
   return (
     <div>
-        {allJobs && Object.keys(allJobs).length > 0 ? (
-            allJobs.map((item, index) => (
-            <div key={index}>
-                <JobCard item={item} />
-            </div>
-            ))
-        ) : (
-            <></>
-        )}
+      {allJobs && Object.keys(allJobs).length > 0 ? (
+        allJobs.map((item, index) => (
+          <div key={index}>
+            <JobCard item={item} />
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
