eb0fe0adfb60d9b7e335e3877afe70097cca9702
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-07-01 04:15:42 +0530

    layout completed for all type of jobs

---
+++ src/app/(dashboard)/opportunities/internship/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Internships
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/(dashboard)/opportunities/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      All opprtunityes
+    </div>
+  )
+}
+
+export default page
